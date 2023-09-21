375f4dd70a4ff8e13525908682ce6440b7681811
thanhtratran <kingrootdm@gmail.com>
2023-09-21 16:31:53 +0700

    ud config close mea Tuan

--- pwn/close_me/chall_config.sh
+++ pwn/close_me/chall_config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "configuring webchallenge-recon on port 8011...";
-docker build -t webchallenge-recon .;
-docker run -d -p 8011:80 webchallenge-recon;
+echo "configuring close-me on port 8011...";
+docker build -t close-me .;
+docker run -d -p 8011:80 cslose-me;
